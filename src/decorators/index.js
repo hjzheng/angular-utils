dbbf8051c1d0462c310ed9b9fe10b71c889ae8eb
hjzheng <zhenghaoju700@163.com>
2016-12-05 10:11:05 +0800

    add InjectServices

--- src/decorators/index.js
+++ src/decorators/index.js
@@ -3,11 +3,13 @@ import { $Apply } from './$Apply';
 import { $Timeout } from './$Timeout';
 import { Mixin } from './Mixin';
 import { Router } from './Router';
+import { InjectServices } from './InjectServices';
 
 export {
 	$Inject,
 	$Timeout,
 	$Apply,
 	Mixin,
-	Router
+	Router,
+	InjectServices
 };
