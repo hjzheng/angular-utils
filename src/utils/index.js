4f81a2d84166dfff5e5cab09e7e3d35842c9c922
hjzheng <zhenghaoju700@163.com>
2016-12-09 17:21:14 +0800

    export spread

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
 import decoratedModule from './decoratedModule';
 import InterceptorFactory from './InterceptorFactory';
+import spread from './spread';
 
 export {
 	decoratedModule,
-	InterceptorFactory
+	InterceptorFactory,
+	spread
 };
